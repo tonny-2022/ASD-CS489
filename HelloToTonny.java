7ad4ad0e3cd8cd3ecd3ffac200425e8fca916723
tonny-2022 <tonny78k@gmail.com>
2024-10-28 17:05:31 -0500

    new file add

---
+++ HelloToTonny.java
@@ -0,0 +1,6 @@
+class HelloToTonny{
+
+    public static void main(string args[]){
+           System.out.println("Hello To Tonny");
+    }
+}
--- Helloworld2.java
+++
@@ -1,9 +0,0 @@
-
-class HelloWorld2{
-
-    public static void main(string args[]){
-           System.out.println("Hello world2");
-           System.out.println("Kaooza Tonny world2");
-
-    }
-}
